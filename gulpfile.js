eb31c09c588ada1b73b936a5e2f90f4eed147831
Stefan Bauer <stefan.bauer@n8d.at>
2017-11-22 00:00:08 +0100

    Added plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-const gulp = require('gulp');
-const mocha = require('gulp-mocha');
+const gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    plumber = require('gulp-plumber');
 
 // Run SASS Unit test
 gulp.task('test', () => {
     return gulp.src('test.js', {
             read: false
         })
+        .pipe(plumber())
         // `gulp-mocha` needs filepaths so you can't have any plugins before it
         .pipe(mocha({
             reporter: 'nyan'
