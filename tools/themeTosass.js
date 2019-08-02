49db84fcfb295c7fe33981734f8643f153fd4a79
Stefan Bauer <stefan.bauer@n8d.at>
2019-08-02 21:20:16 +0200

    Tool to generator extended themeslot variables

---
+++ tools/themeTosass.js
@@ -0,0 +1,38 @@
+const path = require('path'),
+    fs = require('fs'),
+    coreFile = path.join(__dirname, '../src/theme-object.json'),
+    outputFile = path.join(__dirname, '../_uif.theme.scss'),
+    sassPrefix = "$uif-";
+
+if (fs.existsSync(coreFile)) {
+
+    console.log('Exists .... OK');
+
+    let sassFileContent = "";
+
+    const coreThemeFile = JSON.parse(
+        fs.readFileSync(coreFile), 'UTF-8');
+
+    coreThemeKeys = Object.keys(coreThemeFile);
+
+    coreThemeKeys.forEach(key => {
+
+        let uifKey = key.replace('ms-', '');
+        let currentKey = "";
+        if (coreThemeFile[key].indexOf('[') !== -1) {
+            currentKey = sassPrefix + uifKey + ": " + coreThemeFile[key] + ";\r\n";
+        } else {
+            currentKey = sassPrefix + uifKey + ": \"[theme:" + uifKey + ", default:" + coreThemeFile[key] + "]\";\r\n";
+        }
+
+        sassFileContent += currentKey;
+
+    });
+
+    console.log(sassFileContent);
+
+    fs.writeFileSync(outputFile, sassFileContent, 'utf-8');
+
+
+
+}
